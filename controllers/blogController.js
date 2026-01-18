866b81bb4a8ba46d103b0332587fba20681b2a03
symbat27 <242262@astanait.edu.kz>
2026-01-18 18:11:23 +0500

    Implement CRUD operations for blog management

---
+++ controllers/blogController.js
@@ -0,0 +1,73 @@
+const Blog = require("../models/Blog");
+
+exports.createBlog = async (req, res) => {
+  try {
+    const { title, body, author } = req.body;
+
+    if (!title || !body) {
+      return res.status(400).json({
+        message: "Title and body are required"
+      });
+    }
+
+    const blog = await Blog.create({ title, body, author });
+    res.status(201).json(blog);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+exports.getAllBlogs = async (req, res) => {
+  try {
+    const blogs = await Blog.find();
+    res.status(200).json(blogs);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+exports.getBlogById = async (req, res) => {
+  try {
+    const blog = await Blog.findById(req.params.id);
+
+    if (!blog) {
+      return res.status(404).json({ message: "Blog not found" });
+    }
+
+    res.status(200).json(blog);
+  } catch (error) {
+    res.status(400).json({ message: "Invalid ID" });
+  }
+};
+
+exports.updateBlog = async (req, res) => {
+  try {
+    const blog = await Blog.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
+
+    if (!blog) {
+      return res.status(404).json({ message: "Blog not found" });
+    }
+
+    res.status(200).json(blog);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+exports.deleteBlog = async (req, res) => {
+  try {
+    const blog = await Blog.findByIdAndDelete(req.params.id);
+
+    if (!blog) {
+      return res.status(404).json({ message: "Blog not found" });
+    }
+
+    res.status(200).json({ message: "Blog deleted successfully" });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
